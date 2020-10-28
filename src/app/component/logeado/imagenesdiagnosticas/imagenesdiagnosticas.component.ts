f46bc767929f5d5c9171448a711fbe5cc31a5c8b
Angela Garcia <danigarcia_1@hotmail.com>
2020-10-28 10:13:49 -0500

    commit

--- src/app/component/logeado/imagenesdiagnosticas/imagenesdiagnosticas.component.ts
+++ src/app/component/logeado/imagenesdiagnosticas/imagenesdiagnosticas.component.ts
@@ -163,7 +163,7 @@ export class ImagenesdiagnosticasComponent implements OnInit {
         });
     }
 
-    openDialog(datoCambio): void {
+    /*openDialog(datoCambio): void {
         this.dataLock.UserActive = new Userlock();
         this.dataLock.DateActive = datoCambio.pacNum
         if(localStorage.getItem('lock')){
@@ -190,8 +190,52 @@ export class ImagenesdiagnosticasComponent implements OnInit {
                         clearInterval(this.timer);
                     }
                 });
-    }
+    }*/
 
+    openDialog(datoCambio): void {
+        this.dataLock.UserActive = new Userlock();
+        this.dataLock.DateActive = datoCambio.pacNum
+        this.dataLock.UserActive.Documento = this.user.uid;
+        this.dataLock.UserActive.Nombre = this.user.cn;
+        if(localStorage.getItem('lock')){
+            this.bloqueoService.unLockAll()
+        }
+        this.metodo = this.bloqueoService.search('userInfo', this.dataLock.DateActive).subscribe(data => {
+            this.unSubcribeFirebase()
+            if(data.length){
+                this.resultados = data;
+                swal({
+                    title: 'Cita bloqueada',
+                    text: `Esta cita se encuentra bloqueada por  ${this.resultados[0].UserActive.Nombre}`,
+                    icon: 'info',
+                  });
+            } else {
+                this.valor = this.bloqueoService.lock(this.dataLock, 'userInfo');
+                localStorage.setItem('lock', this.valor.key);
+                this.timer = setInterval(() => { this.alertUnlock(); }, this.counter * 60000);
+                const dialogRef = this.dialog.open(CambiocitaImgComponent, {
+                    data: {
+                        myVar: { data: datoCambio },
+                    },
+                    height: '500px'
+                });
+                dialogRef.afterClosed().subscribe(result => {
+                    clearInterval(this.timer);
+                    this.bloqueoService.unLock('userInfo/');
+                    if (result != null) {
+                        datoCambio.estadoCita = result;
+                        this.filters.sort((a, b) => (a.estadoCita > b.estadoCita ? 1 : -1));
+                        this.bloqueoService.unLock('userInfo/');
+                        clearInterval(this.timer);
+                    }
+                    if (result === undefined) {
+                        this.bloqueoService.unLock('userInfo/');
+                        clearInterval(this.timer);
+                    }
+                });
+            }
+        });
+    }
     onSubmit() {
         document.body.scrollTop = document.documentElement.scrollTop = 0;
         const months = this.filtroCitas.get("fechaFinal").value
